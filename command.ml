2a76f2954982b3e5aa14a24f654091d96a352d5b
Barry Wang <rejected@apple.com>
2020-04-21 13:31:01 +0800

    Command Placeholder for build

--- command.ml
+++ command.ml
@@ -1,8 +1,28 @@
-type expr_objects = string list
 
-type column_objects = string list
+type bi_op = 
+    AND
+  | OR
+
+type bi_re = 
+    GT
+  | EQ
+  | LT
+  | GTEQ
+  | LTEQ
+
+type column_object = string
+
+type column_objects = column_object list
 
 type table_name = string
+
+type value_objects = string
+
+type set_objects = (column_object * value_objects) list
+
+type expr_objects = 
+    Expr of column_object * bi_re * value_objects
+  | Binary of bi_op * expr_objects * expr_objects 
 
 type command_verb = 
   | Select of column_objects
@@ -14,8 +34,8 @@ type command_verb =
 type command_subject =   
   | From of table_name
   | Where of expr_objects
-  | Values of expr_objects
-  | Set of expr_objects
+  | Values of value_objects
+  | Set of set_objects
 
 type command_formatter = 
   | OrderBy of column_objects
@@ -23,3 +43,10 @@ type command_formatter =
 
 type command = command_verb * command_subject list 
                * command_formatter list
+
+exception Empty
+
+exception Malformed
+
+
+let parse str = failwith("Unimplemented")
