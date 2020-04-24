a66c62334102c78f4f9ef3dc1b7d2cda5fdba2d7
Leo Moon <jm2239@cornell.edu>
2020-04-24 11:43:20 -0400

    author revised

---
+++ author.mli
@@ -0,0 +1,7 @@
+(**
+   Metadata about this submission.
+   @author Leo Moon (jm2239) Thomas Bastis(tab267) Barry Wang(zw545). 
+*)
+
+(** [hours_worked] is the number of hours you worked on this assignment. *)
+val hours_worked : int
