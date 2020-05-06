e8290b4b1efd174073ccf32040e37790bdd558d4
Leo Moon <jm2239@cornell.edu>
2020-05-05 20:58:41 -0400

    fixed iohandler

--- iohandler.ml
+++ iohandler.ml
@@ -4,7 +4,6 @@ open Sys
 
 let rmv_csv_tl (s:string) = String.sub s 0 (String.length s -4)
 
-
 let is_csv (s:string) : bool =
   let len = String.length s in
   if len < 4 then false
@@ -22,7 +21,7 @@ let csv_array filename = let csv_read = Csv.load filename in
 (**[csv_columns filename] is number rows in a [filename]*)
 let csv_columns filename = Csv.(filename|>load|>lines)
 
-(**[to_csv array_csv] is a csv from csv array representation [array_csv].*)
+(**[to_csv array_csv] is a csv from csv array representation [array_csv]. *)
 let to_csv array_csv = let csv1 = Csv.(array_csv|>of_array)
   in csv1
 
@@ -33,11 +32,18 @@ let save fn array_csv = Csv.save fn (to_csv array_csv)
 let is_dir dir = 
   try is_directory dir with x -> false
 
-let rec add_es_to_htbl tbl = function
+let rec add_data_to_htbl tbl = function
   | [] -> ()
   | h::t -> 
     (Hashtbl.add tbl (rmv_csv_tl h) (csv_array h) );
-    add_es_to_htbl tbl t
+    add_data_to_htbl tbl t
 
-let csvs_in_hastbl dir tbl = 
-  add_es_to_htbl tbl (dir|>all_csv_files)
+(** [csvs_in_hastbl dir tbl] changes first given [dir] to attain [csv] files
+    Once files are attained, comes back to original directory*)
+let csvs_in_hastbl dir tbl =
+  let original_directory = Sys.getcwd () in
+  let file_titles = (dir |> all_csv_files) in
+  Sys.chdir dir;
+  add_data_to_htbl tbl file_titles;
+  Sys.chdir original_directory
+
